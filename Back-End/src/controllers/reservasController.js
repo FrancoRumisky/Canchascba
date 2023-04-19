3f4f0210101c852aca49fdd36fcd6754a6cc3020
FrancoRumiky <framqoo@gmail.com>
2023-04-18 22:14:49 -0300

    fix reservation bug

--- Back-End/src/controllers/reservasController.js
+++ Back-End/src/controllers/reservasController.js
@@ -37,7 +37,7 @@ const findReservation = async (fecha, horaInicio, horaFin) => {
       {
         horaInicio: {
           [Op.gte]: horaInicio,
-          [Op.lt]: horaFin,
+          [Op.lte]: horaFin,
         },
       },
     ],
