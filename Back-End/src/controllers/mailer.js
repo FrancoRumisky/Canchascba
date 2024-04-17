baae9a4aa44bf97632e4d24eeeafdf64bc9b5cf4
FrancoRumiky <framqoo@gmail.com>
2024-04-16 23:49:54 -0300

    enviroment variables mailer

--- Back-End/src/controllers/mailer.js
+++ Back-End/src/controllers/mailer.js
@@ -1,12 +1,14 @@
 const nodemailer = require("nodemailer");
+const NODE_EMAIL = process.env.NODE_EMAIL || 5
+const NODE_PASSWORD = process.env.NODE_PASSWORD || 5
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true, // Use `true` for port 465, `false` for all other ports
   auth: {
-    user: "framqoo@gmail.com",
-    pass: "blbx whpt uilu petn",
+    user: NODE_EMAIL,
+    pass: NODE_PASSWORD,
   },
   tls: { rejectUnauthorized: false },
 });
